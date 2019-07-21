83877da36ae03b4267d5cd54e1718b6dc68acf1b
alptureci90 <alp@accrualify.com>
2019-07-20 18:34:06 -0700

    test

---
+++ src/main/java/processes/RoundRobin.java
@@ -0,0 +1,17 @@
+package processes;
+
+/**
+ Source: https://www.geeksforgeeks.org/program-round-robin-scheduling-set-1/
+ Round Robin is a CPU scheduling algorithm where each process is assigned a fixed time slot in a cyclic way.
+
+ It is simple, easy to implement, and starvation-free as all processes get fair share of CPU.
+ One of the most commonly used technique in CPU scheduling as a core.
+ It is preemptive as processes are assigned CPU only for a fixed slice of time at most.
+ The disadvantage of it is more overhead of context switching.
+ */
+public class RoundRobin {
+
+
+
+
+}
